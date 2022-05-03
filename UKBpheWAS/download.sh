31bc66e91cbc2459f5c03352e808b4e2e6f798a6
Brooke Wolford <bwolford@umich.edu>
2022-05-03 18:03:33 +0300

    Create download.sh

---
+++ UKBpheWAS/download.sh
@@ -0,0 +1,16 @@
+# Download the summary stats
+
+# Check for the lastest manifest
+#i.e. Pan-UK_Biobank_phecode_manifest.tsv
+
+# Create wget for all the results that you want
+# i.e. 
+
+#benb@hunt-phecodes-home:~/work/results/pan-ukbb-v02/phecodes$ head wget_tsv_bgz.sh
+#wget https://pan-ukb-us-east-1.s3.amazonaws.com/sumstats_flat_files/phecode-008-both_sexes.tsv.bgz
+#wget https://pan-ukb-us-east-1.s3.amazonaws.com/sumstats_flat_files/phecode-008.5-both_sexes.tsv.bgz
+
+# Also download the tabix index
+#benb@hunt-phecodes-home:~/work/results/pan-ukbb-v02/phecodes$ head wget_tsv_bgz_tbi.sh
+#wget https://pan-ukb-us-east-1.s3.amazonaws.com/sumstats_flat_files_tabix/phecode-008-both_sexes.tsv.bgz.tbi
+ 
