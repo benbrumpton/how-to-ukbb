77f726905d9af99f2f4d6ea120c00a49a671d9c9
Brooke Wolford <bwolford@hunt-ukbb-home.novalocal>
2022-05-09 09:40:07 +0200

    For now we aren't going to generalize the parsing of the .tab file

--- UKBpheWAS/query.sh
+++ UKBpheWAS/query.sh
@@ -1,14 +1,38 @@
-# Create the regions file to query
-echo "1	11063" > regions.txt
-prefix=1:11063
+#!/bin/bash
 
-# Query the results
-tabix phecode-426.31-both_sexes.tsv.bgz -R regions.txt
+#arguments
+file_dir=$1 #directory with summary stats files which have been tabix and bgzipped with ending *.tsv.bgz, string should not end with "/"
+output=$2
+index=$3 #must be .csv or .tsv
+
+#make sure tabix is a global variable
+
+#do this separately instead of generally for now
+#if [ $index == *".csv" ]; then
+ #   echo "Comma-separated index file provided"
+ #   file=$(cat $index | awk  -v chr='$chr' pos='$pos' '{ print $2"\t"$3 }')
+ #   echo $file > test#
+#elif [ $index == *".tsv" ]; then
+ #    echo "Tab separated index file provided"
+#else
+ #   echo "Index variant file must be .csv or .tsv"
+  #  exit $ERRCODE
+#fi
 
 # Loop through the results and append all to text file
 
-for files in i do
+#remove file so we don't write into it again
+rm $output
 
-tabix phecode-426.31-both_sexes.tsv.bgz -R regions.txt >> UKBpheWAS.${prefix}.results.txt 
-
+for file in `ls $file_dir/*tsv.bgz`
+do
+    echo $file
+    echo $base
+    base=`basename ${file} .tsv.bgz`
+    tabix $file -R $index >> $output
 done
+
+
+#tabix phecode-426.31-both_sexes.tsv.bgz -R regions.txt >> UKBpheWAS.${prefix}.results.txt 
+
+#done
